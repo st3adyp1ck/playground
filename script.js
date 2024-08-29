75e670b1b5235fa0c929158f0ed943a99244a307
st3adyp1ck <160195881+st3adyp1ck@users.noreply.github.com>
2024-08-29 13:29:49 -0400

    String Manipulation Function

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 /*
 CALCULATOR CHALLENGE
@@ -23,29 +22,22 @@ calc(20, 5, "divide");
 calc(10, 0, "divide");
 calc(5, 5, "otherword");
 */
-=======
-"use strict";
+
 /*
-CALCULATOR CHALLENGE
+FILTER AND DOUBLE FUNCTION CHALLENGE
 
-const calc = (a, b, operation) => {
-  const operations = {
-    add: (a, b) => a + b,
-    subtract: (a, b) => a - b,
-    multiply: (a, b) => a * b,
-    divide: (a, b) => (b !== 0 ? a / b : "Cannot Divide By 0"),
-  };
+function filterAndDouble(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const multiply = array[i] * 2;
+    if (array[i] > 10) {
+      newArray.push(multiply);
+    }
+  }
+  return newArray;
+}
 
-  const result = operations[operation]?.(a, b) || "Invalid Operation";
+console.log(filterAndDouble([3, 7, 12, 5, 20]));
+console.log(filterAndDouble([2, 4, 5]));
 
-  console.log(`${a} and ${b} with operation "${operation}" = ${result}`);
-};
-
-calc(10, 10, "add");
-calc(10, 5, "subtract");
-calc(2, 10, "multiply");
-calc(20, 5, "divide");
-calc(10, 0, "divide");
-calc(5, 5, "otherword");
 */
->>>>>>> bee64ba3e567f7c528c16cf5b9ab0ab0f9135053
