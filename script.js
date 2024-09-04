b55b33d73bce542d80f25c682ee83e3ab888f83d
st3adyp1ck <160195881+st3adyp1ck@users.noreply.github.com>
2024-09-04 18:39:20 -0400

    Challenges Updated 09_03

--- script.js
+++ script.js
@@ -2,13 +2,3 @@
 // Array & String Challenges = /js/array_string_challenges.js
 // User ID Challenge = /js/generate_uid.js
 "use strict";
-function hideAllScripts() {
-  document.getElementById("calcScript").disabled = true;
-  document.getElementById("arrayStringScript").disabled = true;
-  document.getElementById("uidScript").disabled = true;
-}
-
-function runScript(scriptId) {
-  hideAllScripts();
-  document.getElementById(scriptId).disabled = false;
-}
